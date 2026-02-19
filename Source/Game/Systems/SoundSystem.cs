0de8a61551d3b8fca147d4f0aa521c23cef29dcd
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2026-02-19 18:12:36 +0100

    feat: add options menu with music volume slider

--- Source/Game/Systems/SoundSystem.cs
+++ Source/Game/Systems/SoundSystem.cs
@@ -1,9 +1,25 @@
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
 namespace Game.Systems;
 
 public class SoundSystem
 {
-    public SoundSystem()
-    {
+    private Music _music;
 
+    public SoundSystem(string musicPath)
+    {
+        _music = LoadMusicStream(musicPath);
+        PlayMusicStream(_music);
+    }
+
+    public void Update()
+    {
+        UpdateMusicStream(_music);
+    }
+
+    public void SetVolume(float volume)
+    {
+        SetMusicVolume(_music, Math.Clamp(volume, 0f, 1f));
     }
 }
