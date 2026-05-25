89c2da3347845584d30e59b55ead14b998993808
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2026-05-25 20:38:44 +0200

    Add Objects.png atlas frame rects for all pickup types.
    Wire Ammo, MachineGun, and key sprites to their sheet coordinates in PickupSprites.

--- Source/Game/Utilities/PickupSprites.cs
+++ Source/Game/Utilities/PickupSprites.cs
@@ -14,9 +14,25 @@ public static class PickupSprites
     public const int HealthX = 66;
     public const int HealthY = 756;
 
+    public const int AmmoX = 1;
+    public const int AmmoY = 829 + 5;
+
+    public const int MachineGunX = 66;
+    public const int MachineGunY = 829 + 5;
+
+    public const int GoldKeyX = 1;
+    public const int GoldKeyY = 989 + 5;
+
+    public const int SilverKeyX = 66;
+    public const int SilverKeyY = 995;
+
     public static Rectangle GetFrameRect(PickupType type) => type switch
     {
         PickupType.Health => new Rectangle(HealthX, HealthY, FrameSize, FrameSize),
+        PickupType.Ammo => new Rectangle(AmmoX, AmmoY, FrameSize, FrameSize),
+        PickupType.MachineGun => new Rectangle(MachineGunX, MachineGunY, FrameSize, FrameSize),
+        PickupType.GoldKey => new Rectangle(GoldKeyX, GoldKeyY, FrameSize, FrameSize),
+        PickupType.SilverKey => new Rectangle(SilverKeyX, SilverKeyY, FrameSize, FrameSize),
         _ => new Rectangle(PlaceholderX, PlaceholderY, FrameSize, FrameSize)
     };
 }
