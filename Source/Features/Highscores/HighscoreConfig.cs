fef88d65ff7da7653fcf69d8ebe93e6006dd7179
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2026-06-20 14:33:10 +0200

    chore: update highscore API base URL to new endpoint

--- Source/Features/Highscores/HighscoreConfig.cs
+++ Source/Features/Highscores/HighscoreConfig.cs
@@ -3,6 +3,6 @@ namespace Game.Features.Highscores;
 /// <summary>Highscore API base URL used by game clients.</summary>
 public static class HighscoreConfig
 {
-    public const string ApiBaseUrl = "https://wolf3d.m0b.tech";
+    public const string ApiBaseUrl = "https://highscores.m0b.tech";
     public const int DefaultTopCount = 10;
 }
