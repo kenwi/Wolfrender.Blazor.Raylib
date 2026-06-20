3c4b277806b67ae8a44900d0b50fb976eaa7e5bb
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2026-06-20 12:39:28 +0200

    chore: update highscore API base URL to production endpoint

--- Source/Features/Highscores/HighscoreConfig.cs
+++ Source/Features/Highscores/HighscoreConfig.cs
@@ -3,6 +3,6 @@ namespace Game.Features.Highscores;
 /// <summary>Highscore API base URL used by game clients.</summary>
 public static class HighscoreConfig
 {
-    public const string ApiBaseUrl = "http://localhost:5080";
+    public const string ApiBaseUrl = "https://highscore.m0b.tech";
     public const int DefaultTopCount = 10;
 }
